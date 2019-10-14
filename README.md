5d41c5c9f93286499d5c046689677b1cb77d47d4
Cablethief <mikek3262@gmail.com>
2019-10-14 13:22:10 +0200

    Fixed to work and returns you to where you came.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Start of a service that allows for a simple SSO login for Traefik forward-auth.
 
-Uses bcrypt to keep files, requires that this is set in the environment for the moment. 
+(Very much beta :D)
+
+Uses bcrypt to keep pass, requires that this is set in the environment for the moment. 
 
 Required environment variables and examples:
 
@@ -20,15 +22,35 @@ go run gen-bcrypt.go -p "P@ssw0rd"
 
 An example traefik config has been provided. 
 
+## Why
+
+I would prefer to login to a single place and have a cookie for all my services, rather than many individual logins. 
 
 ## What it does
 
-The service provides 2 functions. 
+The service provides: 
 
-1. A login page to provide a cookie on successful login for the configured domain. (`/signin`)
-2. A check page which returns a `200 OK` if the cookie is valid. (`/check`)
+1. A page to provide a cookie on successful login for the configured domain.
+2. A page which returns a `200 OK` if the cookie is valid.
 
 The service will also run on `0.0.0.0:8000` 
+
+## How 2
+
+check the provided compose files for more details. set your forward auth to the auth container for the container you are protecting:
+
+```
+        - "traefik.http.middlewares.test-auth.forwardauth.address=http://auth:8000/"
+```
+
+On the auth container setup traefik to handle the auth request. 
+
+```
+        - "traefik.http.routers.auth.rule=Path(`/simple-sso-signin`)"
+        # Required to be the primary rule to handle the creds coming through
+        - "traefik.http.routers.auth.priority=100"
+```
+
 
 ## TODO
 
@@ -36,3 +58,4 @@ The service will also run on `0.0.0.0:8000`
  - Read Users from an htaccess file.
  - Read Users from a traefik basic auth format label.
  - Add options for interface etc. 
+ - Add basic auth option.
--- simple-sso.go
+++ simple-sso.go
@@ -9,64 +9,56 @@ import (
 
 	"github.com/alexedwards/scs/v2"
 	"golang.org/x/crypto/bcrypt"
+	// "github.com/goji/httpauth"
 )
 
 var users = map[string]string{
 	os.Getenv("USERNAME"): os.Getenv("PASSWORD"),
 }
 
-func Signin(res http.ResponseWriter, req *http.Request) {
+// func Check(res http.ResponseWriter, req *http.Request) {
+// 	if sessionManager.Exists(req.Context(), "username") {
+// 		fmt.Fprintf(res, "Welcome!")
+// 	} else {
 
-	switch req.Method {
-	case "GET":
-		http.ServeFile(res, req, "static/index.html")
-	case "POST":
-		if err := req.ParseForm(); err != nil {
-			fmt.Fprintf(res, "ParseForm() err: %v", err)
-			return
-		}
-		username := req.FormValue("username")
-		log.Printf("An auth attempt from %v!\n", username)
-		password := req.FormValue("password")
-
-		// Get the expected password from our in memory map
-		expectedPassword, ok := users[username]
-
-		// If a password exists for the given user
-		// AND, if it is the same as the password we received, the we can move ahead
-		// if NOT, then we return an "Unauthorized" status
-		if !ok || bcrypt.CompareHashAndPassword([]byte(expectedPassword), []byte(password)) != nil {
-			res.WriteHeader(http.StatusUnauthorized)
-			log.Printf("Auth attempt from %v failed!\n", username)
-			return
-		}
-		log.Printf("Auth attempt from %v succeeded!\n", username)
-		sessionManager.Put(req.Context(), "username", os.Getenv("USERNAME"))
-		fmt.Fprintf(res, "Congratz, you authenticated!")
-		http.Redirect(res, req, req.Header.Get("CameFrom"), 302)
-
-	default:
-		fmt.Fprintf(res, "Sorry, only GET and POST methods are supported.")
-
-	}
-
-}
-
-func Check(res http.ResponseWriter, req *http.Request) {
-	if sessionManager.Exists(req.Context(), "username") {
-		fmt.Fprintf(res, "Welcome!")
-	} else {
-		// res.Header().Set("ReferrerPolicy", "unsafe-url")
-		http.Redirect(res, req, "http://auth.local.host:8080/signin", http.StatusSeeOther)
-		// res.WriteHeader(http.StatusUnauthorized)
-	}
-}
+// 		fmt.Fprintf(res, "<script>window.location.pathname = '/simple-sso-signin'</script>\n")
+// 	}
+// }
 
 func Index(res http.ResponseWriter, req *http.Request) {
 	if sessionManager.Exists(req.Context(), "username") {
 		fmt.Fprintf(res, "Welcome!")
 	} else {
-		http.ServeFile(res, req, "./static/index.html")
+		switch req.Method {
+		case "GET":
+			http.ServeFile(res, req, "static/index.html")
+			res.WriteHeader(http.StatusUnauthorized)
+		case "POST":
+			if err := req.ParseForm(); err != nil {
+				fmt.Fprintf(res, "ParseForm() err: %v", err)
+				return
+			}
+			username := req.FormValue("username")
+			log.Printf("An auth attempt from %v!\n", username)
+			password := req.FormValue("password")
+
+			// Get the expected password from our in memory map
+			expectedPassword, ok := users[username]
+
+			// If a password exists for the given user
+			// AND, if it is the same as the password we received, the we can move ahead
+			// if NOT, then we return an "Unauthorized" status
+			if !ok || bcrypt.CompareHashAndPassword([]byte(expectedPassword), []byte(password)) != nil {
+				http.Redirect(res, req, req.Header.Get("referrer"), http.StatusSeeOther)
+				log.Printf("Auth attempt from %v failed!\n", username)
+				return
+			}
+			log.Printf("Auth attempt from %v succeeded!\n", username)
+			sessionManager.Put(req.Context(), "username", os.Getenv("USERNAME"))
+			fmt.Fprintf(res, "Congratz, you authenticated!")
+			http.Redirect(res, req, req.Header.Get("referrer"), http.StatusSeeOther)
+		}
+		// http.ServeFile(res, req, "./static/index.html")
 	}
 }
 
@@ -82,8 +74,7 @@ func main() {
 	fmt.Printf("Will set auth for %v\n", os.Getenv("DOMAIN"))
 	// "Signin" and "Welcome" are the handlers that we will implement
 	mux := http.NewServeMux()
-	mux.HandleFunc("/signin", Signin)
-	mux.HandleFunc("/check", Check)
+	// mux.HandleFunc("/check", Check)
 	mux.HandleFunc("/", Index)
 
 	// start the server on port 8000
